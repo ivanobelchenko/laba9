5307f99da10abe52c5d4aa9baebd992daf92c46b
ivanobelchenko <vanyaobelka@mail.ru>
2021-01-29 18:51:41 +0300

    1

---
+++ script.js
@@ -0,0 +1,13 @@
+//Задание 1
+console.log('Задание 1')
+let student = {
+    firstname: 'Иван',
+    lastname: 'Обельченко',
+    group: '201-321',
+}
+console.log('Список свойств: ' + Object.keys(student).toString());
+console.log('Студент ' + student.firstname + ' '+ student.lastname+ ' учится в '+ student.group+ ' группе' );
+//Задание 2
+console.log("Задание 2");
+
+
